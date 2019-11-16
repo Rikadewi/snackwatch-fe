2df909a857ef1790c5c57b84426aaf7547301cd9
Rika Dewi <rika_4444@yahoo.com>
2019-11-16 22:48:27 +0700

    Fix routing

--- src/route.js
+++ src/route.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import UserNavigator from "./containers/user-navigator";
 import Layout from "./containers/layout";
-import Login from "./containers/login";
 
 class Route extends Component {
 	render() {
 		const { isAuthenticated } = this.props;
 
-		// return !isAuthenticated ? <Login /> : <Layout />;
-		return <Layout />;
+		return !isAuthenticated ? <UserNavigator /> : <Layout />;
+		// return <Layout />;
 	}
 }
 
