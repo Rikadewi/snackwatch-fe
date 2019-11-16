531c90f617d3f7e83beed383e9c5c05d96cdbac6
Rika Dewi <rika_4444@yahoo.com>
2019-11-16 18:41:41 +0700

    Styling allergy page

--- src/containers/register/allergy.js
+++ src/containers/register/allergy.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
 import { addAllergy } from '../../actions/user'
-import { TextInput, Text, View, Image, StyleSheet, ScrollView, SafeAreaView, TouchableHighlight } from 'react-native';
+import { Text, View, Image, ScrollView, SafeAreaView, TouchableHighlight } from 'react-native';
 import { styles } from '../../styles/styles'
-import { H6, GREEN, GRAY, DARK_GRAY, BLACK } from '../../styles/const';
+import { GREEN, GRAY, BLACK } from '../../styles/const';
 import RegisterBar from '../../components/register-bar';
+import NarrowButton from '../../components/narrow-button';
 
 class Allergy extends Component {
     constructor(props) {
@@ -13,18 +14,49 @@ class Allergy extends Component {
             allergies: [],
         };
 
-
+        this.handleChangeAllergy = this.handleChangeAllergy.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleSubmit = () => {
-        console.log("tes");
         this.props.handleAddAllergy(this.state.allergies);
-        console.log(this.state.allergy)
+        this.props.navigation.navigate('Illness');
     }
 
     handleChangeAllergy = (allergy) => {
-        this.setState({ allergies: allergies.push(allergy) });
+        let allergy_encoded = 0;
+        switch (allergy) {
+            case 'Egg':
+                allergy_encoded = 1;
+                break;
+            case 'Peanut':
+                allergy_encoded = 2;
+                break;
+            case 'Tree Nuts':
+                allergy_encoded = 3;
+                break;
+            case 'Soy':
+                allergy_encoded = 4;
+                break;
+            case 'Cow Milk':
+                allergy_encoded = 5;
+                break;
+            case 'Wheat':
+                allergy_encoded = 6;
+                break;
+            case 'Fish':
+                allergy_encoded = 7;
+                break;
+            case 'Shell Fish':
+                allergy_encoded = 8;
+                break;
+            case 'Caffeine':
+                allergy_encoded = 9;
+                break;
+            default:
+                allergy_encoded = 0;
+        }
+        this.state.allergies.push(allergy_encoded);
     }
 
     render() {
@@ -35,12 +67,25 @@ class Allergy extends Component {
                         <RegisterBar count={2} />
                         <Text style={styles.h2}>Do you have any type {"\n"}
                             of food allergy?</Text>
-                        <View style={{
-                            margin: 30,
-                        }}>
-                            <ClickableImage handle={this.handleChangeAllergy} />
-                        </View>
-
+                        <ClickableOption
+                            handle={this.handleChangeAllergy}
+                            handleSubmit={this.handleSubmit}
+                            path='allergy'
+                            options={[
+                                'Egg',
+                                'Peanut',
+                                'Tree Nuts',
+                                'Soy',
+                                'Cow Milk',
+                                'Wheat',
+                                'Fish',
+                                'Shell Fish',
+                                'Caffeine'
+                            ]}
+                        />
+                        <NarrowButton
+                            onPress={this.handleSubmit}
+                            text="Continue" />
                     </View>
                 </ScrollView>
             </SafeAreaView >
@@ -48,34 +93,90 @@ class Allergy extends Component {
     }
 };
 
-class ClickableImage extends Component {
+class ClickableOption extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
+        let options = [];
+        for (let i = 0; i < this.props.options.length; i++) {
+            options.push(
+                <ClickableImage handle={this.props.handle.bind(this)} name={this.props.options[i]} path={this.props.path} />
+            )
+        }
         return (
-            <View>
-                <TouchableHighlight
-                    onPress={this.handle}
-                    style={{
-                        backgroundColor: GRAY,
-                        paddingHorizontal: 14.5,
-                        paddingVertical: 14.5,
-                        borderRadius: 72,
-                    }}
-                >
-                    <Image
-                        source={require('../../static/allergy/Egg.png')}
-                        style={{
-                            width: 43,
-                            height: 43,
-                        }}
-                    />
-                </TouchableHighlight>
-                <Text style={{
-                    color: BLACK,
-                    textAlign: 'center',
-                    marginTop: 2,
-                }}>Egg</Text>
+            <View style={{
+                margin: 30,
+                marginBottom: 10,
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                justifyContent: 'space-between',
+                width: 261,
+            }}>
+                {options}
+                <ClickableImage handle={this.props.handleSubmit.bind(this)} name="No" path={this.props.path} />
             </View>
         );
+    }
+}
+
+class ClickableImage extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        if (this.props.name == 'No') {
+            return (
+                <View style={{ marginBottom: 19 }}>
+                    <TouchableHighlight
+                        onPress={this.props.handle}
+                        style={{
+                            backgroundColor: GREEN,
+                            width: 72,
+                            height: 72,
+                            borderRadius: 72,
+                        }}
+                    >
+                        <Text style={{
+                            color: '#ffffff',
+                            textAlign: 'center',
+                            fontSize: 28,
+                            lineHeight: 72,
+                        }}>{this.props.name}</Text>
+                    </TouchableHighlight>
+
+                </View >
+            );
+        } else {
+            return (
+                <View style={{ marginBottom: 19 }}>
+                    <TouchableHighlight
+                        onPress={() => this.props.handle(this.props.name)}
+                        style={{
+                            backgroundColor: GRAY,
+                            paddingHorizontal: 14.5,
+                            paddingVertical: 14.5,
+                            borderRadius: 72,
+                        }}
+                    >
+                        <Image
+                            source={require('../../static/' + this.props.path + '/' + this.props.name + '.png')}
+                            style={{
+                                width: 43,
+                                height: 43,
+                            }}
+                        />
+                    </TouchableHighlight>
+                    <Text style={{
+                        color: BLACK,
+                        textAlign: 'center',
+                        marginTop: 2,
+                    }}>{this.props.name}</Text>
+                </View>
+            );
+        }
     }
 }
 
--- src/containers/user-navigator.js
+++ src/containers/user-navigator.js
@@ -4,12 +4,14 @@ import Login from './login';
 import Hello from './hello';
 import Know from './register/know';
 import Allergy from './register/allergy';
+import Illness from './register/illness';
 
 const UserNavigator = createStackNavigator(
     {
-        Allergy: { screen: Allergy },
         Hello: { screen: Hello },
         Know: { screen: Know },
+        Allergy: { screen: Allergy },
+        Illness: { screen: Illness },
         Login: { screen: Login },
     },
     {
